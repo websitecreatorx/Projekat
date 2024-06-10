1694b31a8d768ff24a0d796d86b0cb44a59f8a45
drip <118680430+websitecreatorx@users.noreply.github.com>
2024-06-10 18:28:39 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,30 +1,64 @@
-const wrapperslider = document.getElementById('news-slider');
+// News Wrapper Slider Automation 
 
-function scrollWrapperSlider(){
-    const scrollAmount = amount;
-    wrapperslider.scrollBy(scrollAmount, 0);   
-    if(wrapperslider.scrollLeft + wrapperslider.clientWidth >= wrapperslider.scrollWidth - 10){
-        clearInterval(timer);
-        setTimeout(scrollBackWrapperSlider, 0);
+const NewsWrapperSlider = document.getElementById('news-slider');
+
+function scrollNewsWrapperSlider(){
+    const NewsScrollAmount = NewsAmount;
+    NewsWrapperSlider.scrollBy(NewsScrollAmount, 0);   
+    if(NewsWrapperSlider.scrollLeft + NewsWrapperSlider.clientWidth >= NewsWrapperSlider.scrollWidth - 10){
+        clearInterval(NewsTimer);
+        setTimeout(scrollBackNewsWrapperSlider, 0);
     }
 }
 
-function scrollBackWrapperSlider(){
-    wrapperslider.scrollLeft = 0;
-    if(wrapperslider.scrollLeft == 0){
-        setInterval(scrollWrapperSlider, 7000);
+function scrollBackNewsWrapperSlider(){
+    NewsWrapperSlider.scrollLeft = 0;
+    if(NewsWrapperSlider.scrollLeft == 0){
+        setInterval(scrollNewsWrapperSlider, 7000);
     }
 }
 
-wrapperslider.addEventListener('onload', scrollBackWrapperSlider());
-wrapperslider.addEventListener('onload', timer);
+NewsWrapperSlider.addEventListener('onload', scrollBackNewsWrapperSlider());
+NewsWrapperSlider.addEventListener('onload', NewsTimer);
 
-var timer = setInterval(scrollWrapperSlider, 7000);
+var NewsTimer = setInterval(scrollNewsWrapperSlider, 7000);
 
-let amount;
+let NewsAmount;
 
-if(window.innerWidth < 900){
-    amount = (wrapperslider.scrollWidth - wrapperslider.clientWidth ) / 4;
+if(window.innerWidth >= 900){
+    NewsAmount = (NewsWrapperSlider.scrollWidth - NewsWrapperSlider.clientWidth ) / 2 + 1;
 }else{
-    amount = (wrapperslider.scrollWidth - wrapperslider.clientWidth ) / 2 + 1;
+    NewsAmount = (NewsWrapperSlider.scrollWidth - NewsWrapperSlider.clientWidth ) / 4;
 }
+
+// Partners Wrapper Slider Automation
+
+const PartnersWrapperSlider = document.getElementById('partners-slider');
+
+function scrollPartnersWrapperSlider(){
+    const PartnersScrollAmount = PartnersAmount;
+    PartnersWrapperSlider.scrollBy(PartnersScrollAmount, 0);
+    if(PartnersWrapperSlider.scrollLeft + PartnersWrapperSlider.clientWidth >= PartnersWrapperSlider.scrollWidth - 10){
+        clearInterval(PartnersTimer);
+        setTimeout(scrollBackPartnersWrapperSlider, 0);
+    }
+}
+
+function scrollBackPartnersWrapperSlider(){
+    PartnersWrapperSlider.scrollLeft = 0;
+    if(PartnersWrapperSlider.scrollLeft == 0){
+        setInterval(scrollPartnersWrapperSlider, 7000);
+    }
+}
+
+PartnersWrapperSlider.addEventListener('onload', scrollBackPartnersWrapperSlider());
+PartnersWrapperSlider.addEventListener('onload', PartnersTimer);
+
+var PartnersTimer = setInterval(scrollPartnersWrapperSlider, 7000);
+
+let PartnersAmount;
+
+if(window.innerWidth >= 1920){
+    PartnersAmount = 1806;
+}
+
